fc24f3d256fc778588319ea2be25b8f1ba9b5af7
ravigordon <rgordz460@outlook.com>
2022-02-24 16:57:47 +0000

    card rows corrected

--- my-app/Components/Card/index.js
+++ my-app/Components/Card/index.js
@@ -2,13 +2,13 @@ import { Card,Col, Row} from "react-bootstrap";
 
 const Vegcard = ({ data }) => {
   return (
-    <Row xs={2} s={2} md={3} lg={3}  className="g-4">
+    <Row xs={1} s={1} md={2} lg={3}  className="g-4">
   {Array.from({ length: 6 }).map((_,idx ) => (
     <Col>
       <Card>
-        <Card.Img variant="top"  />
+      <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
-          <Card.Title>Iteme</Card.Title>
+          <Card.Title>Item</Card.Title>
           <Card.Text>
               Tastes soo good
               <br/>
