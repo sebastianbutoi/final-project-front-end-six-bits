e9ed5c387a2829b549694a49009bc3fd87fbff00
jafcoder <joefarr113@gmail.com>
2022-02-24 11:56:56 +0000

    changed to use environment variables

--- my-app/pages/browse.js
+++ my-app/pages/browse.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import CardDisplay from "../Components/Card";
 import css from "../styles/browse.module.css";
-import Footer from "../Components/Footer";
+
 import OptionsBar from "../Components/OptionsBar";
 
 const URL = process.env.NEXT_PUBLIC_API_URL;
@@ -10,14 +10,10 @@ function Browse() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-
     async function fetchData() {
-      const res = await fetch(
-        "https://sixbits-project.herokuapp.com/api/posts"
-      );
+      const res = await fetch(`${URL}/posts`);
       const data = await res.json();
       setData([...data.payload]);
-
     }
     fetchData();
   }, []);
